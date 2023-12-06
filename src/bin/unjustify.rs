d5dfddc9d068e64c2bf5cf17f114e4645f717b93
Daniel Bergey <daniel.bergey@braze.com>
2023-12-06 18:30:00 -0500

    --whitespace

--- src/bin/unjustify.rs
+++ src/bin/unjustify.rs
@@ -1,19 +1,70 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use std::io;
 use std::io::BufRead;
+use std::fmt;
+
+#[derive(Debug, Clone, ValueEnum)]
+enum SplitWhitespace {
+    Any,
+    Double,
+    Ignore,
+}
+
+impl fmt::Display for SplitWhitespace {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        use SplitWhitespace::*;
+        match self {
+            Any => write!(out, "{}", "any"),
+            Double => write!(out, "{}", "double"),
+            Ignore => write!(out, "{}", "ignore"),
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[arg(long, short, help="additional column delimiters", default_value="")]
     delimiters: String,
+    #[arg(long, short, help="whitespace delimited?", default_value_t=SplitWhitespace::Any)]
+    whitespace: SplitWhitespace,
 }
 
-fn update_spaces(delimiters: &str, mut spaces: Vec<bool>, string: &String) -> Vec<bool> {
-    for (i, c) in string.chars().enumerate() {
+impl Default for Cli {
+    fn default() -> Self {
+        Cli {
+            delimiters: "".to_string(),
+            whitespace: SplitWhitespace::Any,
+        }
+    }
+}
+
+fn some_whitespace(c: Option<char>) -> bool {
+    match c {
+        Some(c) => c.is_whitespace(),
+        None => false,
+    }
+}
+   
+
+fn is_delimiter(args: &Cli, before: Option<char>, c: char, after: Option<char>) -> bool {
+    let matches_whitespace = match args.whitespace {
+        SplitWhitespace::Any => c.is_whitespace(),
+        SplitWhitespace::Ignore => false,
+        SplitWhitespace::Double => c.is_whitespace() && (some_whitespace(before) || some_whitespace(after))
+    };
+    let matches_delimiters = args.delimiters.contains(c);
+    matches_whitespace || matches_delimiters
+}
+
+fn update_spaces(args: &Cli, mut spaces: Vec<bool>, string: &String) -> Vec<bool> {
+    let chars: Vec<char> = string.chars().collect();
+    for (i, c) in chars.iter().enumerate() {
+        let before = if i > 0 { Some(chars[i-1]) } else { None };
+        let after = if i + 1 < chars.len() { Some(chars[i+1])} else { None };
         match spaces.get_mut(i) {
-            Some(space) => *space = *space && (c.is_whitespace() || delimiters.contains(c)),
-            None => spaces.push(c.is_whitespace() || delimiters.contains(c))
+            Some(space) => *space = *space && is_delimiter(args, before, *c, after),
+            None => spaces.push(is_delimiter(args, before, *c, after)),
         }
     }
     spaces
@@ -44,7 +95,7 @@ fn main() -> io::Result<()> {
     let in_handle = stdin.lock();
 
     let lines = in_handle.lines().collect::<io::Result<Vec<String>>>()?;
-    let spaces = lines.iter().fold(Vec::new(), |spaces, string| { update_spaces(&args.delimiters, spaces, string) });
+    let spaces = lines.iter().fold(Vec::new(), |spaces, string| { update_spaces(&args, spaces, string) });
     let columns = columns(&spaces);
 
     let mut outln: Vec<String> = Vec::new();
@@ -68,35 +119,70 @@ pub mod tests {
     fn update_spaces_first_line() {
         let mut line = String::new();
         line.push_str("  a bb  ccc ");
-        let spaces = update_spaces("", Vec::new(), &line);
+        let spaces = update_spaces(&Default::default(), Vec::new(), &line);
         assert_eq!(spaces, vec![true, true, false, true, false, false, true, true, false, false, false, true])
     }
 
     #[test]
     fn update_spaces_two_lines() {
+        let args = Default::default();
         let mut line = String::new();
         line.push_str("  a bb  ccc ");
-        let spaces = update_spaces("", Vec::new(), &line);
+        let spaces = update_spaces(&args, Vec::new(), &line);
         line.clear();
         line.push_str(" aa  b ccc  ");
-        let spaces = update_spaces("", spaces, &line);
+        let spaces = update_spaces(&args, spaces, &line);
         assert_eq!(spaces, vec![true, false, false, true, false, false, true, false, false, false, false, true])
     }
 
     #[test]
     fn update_spaces_comma() {
+        let mut args: Cli = Default::default();
+        args.delimiters.push_str(",");
         let mut line = String::new();
         line.push_str(",,a,bb,,ccc,");
-        let spaces = update_spaces(",", Vec::new(), &line);
+        let spaces = update_spaces(&args, Vec::new(), &line);
         assert_eq!(spaces, vec![true, true, false, true, false, false, true, true, false, false, false, true])
     }
 
     #[test]
     fn update_spaces_mixed_delims() {
+        let mut args: Cli = Default::default();
+        args.delimiters.push_str(",");
         let mut line = String::new();
         line.push_str(", a,bb, ccc,");
-        let spaces = update_spaces(",", Vec::new(), &line);
+        let spaces = update_spaces(&args, Vec::new(), &line);
         assert_eq!(spaces, vec![true, true, false, true, false, false, true, true, false, false, false, true])
+    }
+
+    #[test]
+    fn update_spaces_double() {
+        let mut args: Cli = Default::default();
+        args.whitespace = SplitWhitespace::Double;
+        let mut line = String::new();
+        line.push_str(" a  b b   c c ");
+        let spaces = update_spaces(&args, Vec::new(), &line);
+        assert_eq!(spaces, vec![false, false, true, true, false, false, false, true, true, true, false, false, false, false])
+    }
+
+    #[test]
+    fn update_spaces_double_ends() {
+        let mut args: Cli = Default::default();
+        args.whitespace = SplitWhitespace::Double;
+        let mut line = String::new();
+        line.push_str("  a  b b   c c  ");
+        let spaces = update_spaces(&args, Vec::new(), &line);
+        assert_eq!(spaces, vec![true, true, false, true, true, false, false, false, true, true, true, false, false, false, true, true])
+    }
+
+    #[test]
+    fn update_spaces_double_starts() {
+        let mut args: Cli = Default::default();
+        args.whitespace = SplitWhitespace::Double;
+        let mut line = String::new();
+        line.push_str("a  b b   c c");
+        let spaces = update_spaces(&args, Vec::new(), &line);
+        assert_eq!(spaces, vec![false, true, true, false, false, false, true, true, true, false, false, false])
     }
 
     #[test]
